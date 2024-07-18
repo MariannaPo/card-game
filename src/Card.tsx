c24a1e8e9b59fc9180ddadf8a3ba7caa11b032cb
Marianna_Po <mapodmasko@gmail.com>
2024-07-18 19:03:19 +0300

    correct color

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,28 +1,54 @@
 import { type } from "os"
 import { CardType } from "./App"
 import s from './card.style.module.css'
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { idText } from "typescript"
 
 
 export type CardPropsType={
     cards: CardType[],
-    // clickCardHandler: (id: number)=>void
 }
 
 export const Card=({cards}: CardPropsType)=>{
 
-    const [color, setColor] = useState();
-    const [selectedCardId, setSelectedCardId] = useState<number | null>(null);
-  
-    const clickCardHandler=(id: number)=>{
-          setSelectedCardId(id)
+    const [selectedCardsIds, setSelectedCardsIds] = useState<number[]>([])
+    const [guessesCards, setGuessesCards] = useState<number[]>([])
+
+
+    const clickCardHandler = (id: number) => {
+        if (selectedCardsIds.includes(id)) {
+            const filteredIds = selectedCardsIds.filter((selectedId) => selectedId !== id);
+            setSelectedCardsIds(filteredIds);
+        } else {
+            setSelectedCardsIds([...selectedCardsIds, id]);
         }
+        
+        // const winCard=()=>{
+        //     if(selectedCardsIds)
+        
+        //         console.log(id)
+        //     }
+        //     winCard()
+    };
+
+//    const winCard=(id: number)=>{
+//     if(cards.id === id)
+//         console.log(id)
+//     }
+
+
+    const colorChange=(a: any, id: number, color: string)=>{
+    if(selectedCardsIds.includes(a.id)){
+        return  a.color 
+    }
+    }
+
 
     return(
         <div className={s.item}>
             {cards.map((card) => ( 
                 <div key={card.id} 
-                style={{ backgroundColor: card.id === selectedCardId ? card.color : 'white' }}
+                style={{ backgroundColor: colorChange(card, card.id, card.color)}}
                 className={s.cardDiv} 
                 onClick={()=>clickCardHandler(card.id)}>
                     {card.color}
